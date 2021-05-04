810f96baf67e4810ca0130fa07e44062f2227283
Norbert Nawrotek <norbertn15@gmail.com>
2021-05-04 04:48:17 +0200

    Run example game on start

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from game_controller import GameController, Player
+from human_player import HumanPlayer
 
 if __name__ == "__main__":
-    game_controller = GameController(Player(), Player())
+    game_controller = GameController(HumanPlayer(), HumanPlayer())
+    game_controller.run_game()
