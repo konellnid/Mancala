3fa6bdcf743bc0b65ed421ed43e956660f6f277f
Norbert Nawrotek <norbertn15@gmail.com>
2021-05-04 06:22:14 +0200

    Organize main file

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from random_move_player import RandomMovePlayer
 if __name__ == "__main__":
     default_evaluation_function_settings = EvaluationFunctionSettings()
     evaluation_function = EvaluationFunction(default_evaluation_function_settings)
+
     game_controller = GameController(MinMaxPlayer(2, evaluation_function),
                                      MinMaxPlayer(5, evaluation_function))
     # game_controller = GameController(HumanPlayer(), HumanPlayer())
     # game_controller = GameController(RandomMovePlayer(), RandomMovePlayer())
+    # just choose any two players
+
     game_controller.run_game()
