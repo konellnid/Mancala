7478f2dca951f725ae4ccfe6db8dc9b81be463e0
Norbert Nawrotek <norbertn15@gmail.com>
2021-05-11 01:11:17 +0200

    Add option to make first move random

--- game_controller.py
+++ game_controller.py
@@ -1,4 +1,5 @@
 import copy
+import random
 
 from mancala import MancalaPosition, STARTING_BOARD
 from typing import List
@@ -11,13 +12,25 @@ class Player:
         pass
 
 
+class GameControllerSettings:
+    def __init__(self, should_make_first_random_move=True):
+        self.should_make_first_random_move = should_make_first_random_move
+
+
 class GameController:
-    def __init__(self, player_a: Player, player_b: Player):
+    def __init__(self, player_a: Player, player_b: Player, game_controller_settings: GameControllerSettings):
         self.player_a = player_a
         self.player_b = player_b
+        self.game_controller_settings = game_controller_settings
         self.position: MancalaPosition = MancalaPosition(STARTING_BOARD, IS_PLAYER_A_STARTING)
 
     def run_game(self):
+        if self.game_controller_settings.should_make_first_random_move:
+            possible_moves = self.position.get_possible_moves()
+            random_first_move = random.choice(possible_moves)
+            self.position = self.position.get_position_after_move(random_first_move)
+            print('First random move made')
+
         while not self.position.is_game_finished():
             possible_moves = self.position.get_possible_moves()
             possible_moves_copy = copy.deepcopy(possible_moves)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from game_controller import GameController, Player
+from game_controller import GameController, Player, GameControllerSettings
 from human_player import HumanPlayer
 from min_max_player import MinMaxPlayer
 from evaluation_function import *
@@ -8,8 +8,9 @@ if __name__ == "__main__":
     default_evaluation_function_settings = EvaluationFunctionSettings()
     evaluation_function = EvaluationFunction(default_evaluation_function_settings)
 
+    game_controller_settings = GameControllerSettings(should_make_first_random_move=True)
     game_controller = GameController(MinMaxPlayer(2, evaluation_function),
-                                     MinMaxPlayer(5, evaluation_function))
+                                     MinMaxPlayer(5, evaluation_function), game_controller_settings)
     # game_controller = GameController(HumanPlayer(), HumanPlayer())
     # game_controller = GameController(RandomMovePlayer(), RandomMovePlayer())
     # just choose any two players
