c27f24858312dfcd03f57dc40ee5f07a7ff5b9a6
Norbert Nawrotek <norbertn15@gmail.com>
2021-05-16 21:10:58 +0200

    Add saving game infos to file

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import pickle
+
 from game_controller import GameController, Player, GameControllerSettings
 from human_player import HumanPlayer
 from min_max_player import MinMaxPlayer
 from evaluation_function import *
 from aplha_beta_player import *
 from random_move_player import RandomMovePlayer
+
+
+def save_game_infos(infos_to_save, title):
+    with open('saved/' + title, 'wb') as solutions_file:
+        pickle.dump(infos_to_save, solutions_file)
+
+
+def open_saved_infos(title):
+    with open('saved/' + title, 'rb') as solving_infos_file:
+        infos_to_load = pickle.load(solving_infos_file)
+
+        return infos_to_load
+
 
 if __name__ == "__main__":
     default_evaluation_function_settings = EvaluationFunctionSettings()
@@ -20,3 +35,7 @@ if __name__ == "__main__":
     game_data = game_controller.run_game()
 
     game_data.print_game_info()
+
+    infos = [game_data]
+    save_game_infos(infos, 'minmax_3-ab_3')
+    infos_loaded = open_saved_infos('minmax_3-ab_3')
